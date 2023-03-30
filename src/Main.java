7cf2739b74875d30cf19fb5c0edd4d302da47d89
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-29 23:29:20 -0500

    conteo de vocales de una cadena

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String cadena = "Huanuco con la mejor clima del mundo";
+        int contador=0;
+        for(int i=0;i<cadena.length();i++){
+            if(cadena.charAt(i)=='a'||
+                    cadena.charAt(i)=='e'||
+                    cadena.charAt(i)=='i'||
+                    cadena.charAt(i)=='o'||
+                    cadena.charAt(i)=='u'){
+                contador=contador+1;
+            }
+        }
+        System.out.println("Las vocales son: "+contador);
+    }
+}
